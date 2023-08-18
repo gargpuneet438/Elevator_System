a1ca0bb5dedb0dc24972c6399a471dae49515a83
Puneet Garg <43969940+gargpuneet438@users.noreply.github.com>
2023-08-18 23:00:17 +0530

    added models.py and serializers.py file

---
+++ src/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class ElevatorSystem(models.Model):
+   
+    system_name = models.CharField(max_length=25)
+    total_floors = models.PositiveIntegerField()
+    number_of_elevators = models.PositiveIntegerField()
+    
+    def __str__(self) -> str:
+        return f"{self.system_name} Elevator System Number {self.id}"
+
+
+class Elevator(models.Model):
+   
+    class RunningStatus(models.IntegerChoices):
+        GOING_UP = 1
+        NOT_MOVING = 0
+        GOING_DOWN = -1
+    
+    elevator_system = models.ForeignKey(ElevatorSystem, on_delete=models.CASCADE)
+    elevator_number = models.IntegerField()
+    current_floor = models.PositiveSmallIntegerField(default=0)
+    is_operational = models.BooleanField(default=True)
+    is_door_open = models.BooleanField(default=True)
+    running_status = models.IntegerField(choices=RunningStatus.choices, default=RunningStatus.NOT_MOVING)
+    
+    def __str__(self) -> str:
+        return f"Elevator Number {self.elevator_number}"
+
+
+class ElevatorRequest(models.Model):
+   
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE)
+    source_floor = models.PositiveSmallIntegerField()
+    destination_floor = models.PositiveSmallIntegerField()
+    request_time = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"{self.elevator} is requested at floor {self.source_floor}"
