4fe9578e5a0c429aa0dc49ba4f91209d3ffdc481
Puneet Garg <43969940+gargpuneet438@users.noreply.github.com>
2023-08-19 17:04:42 +0530

    added urls.py and views.py

---
+++ src/views.py
@@ -0,0 +1,110 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+
+from .models import Elevator, ElevatorRequest, ElevatorSystem
+from .serializers import ElevatorRequestFullSerializer, ElevatorRequestSerializer, ElevatorSerializer, ElevatorSystemSerializer
+from utils.initiate_elevators import initiate_elevators
+
+class ElevatorSystemViewSet(viewsets.ModelViewSet):
+    """
+        This Django view generates an elevator system, starts elevators, and displays the elevators connected to 
+        a specific elevator system.
+
+        
+        """
+    queryset = ElevatorSystem.objects.all()
+    serializer_class = ElevatorSystemSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        elevator_system = serializer.save()
+        initiate_elevators(
+            number_of_elevators=serializer.data['number_of_elevators'],
+            system_id=elevator_system.id
+        )
+        return Response(serializer.data)
+
+
+    @action(detail=True, methods=['get'])
+    def show_elevators(self, request, pk=None):
+        system_id = self.kwargs['pk']
+        elevators = Elevator.objects.filter(elevator_system_id=system_id)
+        serializer = ElevatorSerializer(elevators, many=True)
+        return Response(serializer.data)
+
+
+class ElevatorViewSet(viewsets.ModelViewSet):
+    """
+        This Django viewset has actions for displaying, updating, obtaining the destination, 
+        obtaining the status of requests, sending requests, and obtaining the proper serializer class.
+
+
+        
+    """
+    queryset = Elevator.objects.all()
+    serializer_class = ElevatorSerializer
+
+    @action(detail=True, methods=['get'])
+    def show(self, request, id=None, pk=None):
+        system_id = self.kwargs['id']
+        elevator_number = self.kwargs['pk']
+        elevator = Elevator.objects.get(elevator_system_id=system_id, elevator_number=elevator_number)
+        serializer = ElevatorSerializer(elevator)
+        return Response(serializer.data)
+
+
+    @action(detail=True, methods=['put', 'patch'])
+    def custom_update(self, request, id=None, pk=None):
+        system_id = self.kwargs['id']
+        elevator_number = self.kwargs['pk']
+        elevator = Elevator.objects.get(elevator_system__id=system_id, elevator_number=elevator_number)
+        serializer = ElevatorSerializer(elevator, data=request.data, partial=True)  # `partial=True` allows partial updates
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+
+    @action(detail=True, methods=['get'])
+    def destination(self, request, id=None, pk=None):
+        elevator = self.get_object()
+
+        if not elevator.is_operational:
+            return Response({'running': False, 'details': 'The Elevator is not operational'})
+
+        requests_pending = ElevatorRequest.objects.filter(elevator=elevator, is_active=True).order_by('request_time')
+
+        if requests_pending.count() == 0:
+            return Response({'running': False, 'details': 'The Elevator is not running currently, No pending requests'})
+
+        if requests_pending[0].requested_floor == elevator.current_floor:
+            return Response({'running': True, 'details': str(requests_pending[0].destination_floor)})
+
+        return Response({'running': True, 'details': str(requests_pending[0].requested_floor)})
+
+        
+    @action(detail=True, methods=['get'])
+    def req_current_status(self, request, id=None, pk=None):
+        elevator = self.get_object()
+        elevator_requests = elevator.elevatorrequest_set.all()
+        serializer = ElevatorRequestFullSerializer(elevator_requests, many=True)
+        return Response(serializer.data)
+
+    
+    @action(detail=True, methods=['post'])
+    def make_request(self, request, id=None, pk=None):
+        elevator = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(elevator=elevator)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+    def get_serializer_class(self):
+        if self.action == 'make_request':
+            return ElevatorRequestSerializer
+        return super().get_serializer_class()
